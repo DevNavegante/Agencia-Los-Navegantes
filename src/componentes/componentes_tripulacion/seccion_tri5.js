437673c19dd23c7c88cff6d962ac4160835ffc3d
DevNavegante <s.godoy@agencialosnavegantes.cl>
2025-11-13 17:44:02 -0300

    Update seccion_tri5.js

--- src/componentes/componentes_tripulacion/seccion_tri5.js
+++ src/componentes/componentes_tripulacion/seccion_tri5.js
@@ -7,7 +7,7 @@ function SeccionTri5() {
   const data = response.allStrapiTripulacion.nodes[0].tripulacion_seccion6[0];
   const titulo = data.titulo;
   const descripcion = data.descripcion;
-  const imagen = data.imagen[0].url;
+  //const imagen = data.imagen[0].url;
 
   return (
     <div  className="contenedor-body6">
