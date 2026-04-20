23f33e6da0c74ef0f9bbadc06e9dd3a7df468ad8
DevNavegante <s.godoy@agencialosnavegantes.cl>
2026-04-20 12:09:51 -0400

    Update sliderReviews.js

--- src/componentes/componentes_home/sliderReviews.js
+++ src/componentes/componentes_home/sliderReviews.js
@@ -470,7 +470,27 @@ alt="Agencia Los Navegantes"
                   src="https://cdn.shopify.com/s/files/1/0610/5648/3573/files/AGENCIA_-_Testimonios-23.webp?v=1767099425"                               
                   ></img>
  </SwiperSlide>
-            
+            <SwiperSlide>  
+  <img                  
+                  alt="Agencia Los Navegantes"
+                  className="mmimm lazyload img-fluid"
+                  src="https://cdn.shopify.com/s/files/1/0610/5648/3573/files/AGENCIA_-_Testimonios.webp?v=1775072508"                               
+                  ></img>
+ </SwiperSlide>
+            <SwiperSlide>  
+  <img                  
+                  alt="Agencia Los Navegantes"
+                  className="mmimm lazyload img-fluid"
+                  src="https://cdn.shopify.com/s/files/1/0610/5648/3573/files/AGENCIA_-_Testimonios_AN-03.webp?v=1776700136"                               
+                  ></img>
+ </SwiperSlide>
+            <SwiperSlide>  
+  <img                  
+                  alt="Agencia Los Navegantes"
+                  className="mmimm lazyload img-fluid"
+                  src="https://cdn.shopify.com/s/files/1/0610/5648/3573/files/AGENCIA_-_Testimonios_AN-02.webp?v=1776700136"                               
+                  ></img>
+ </SwiperSlide>
       </Swiper>
 
       </div>
